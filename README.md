ba1f2ca985e663b17f8585dc42ba7c06417ed898
aibarr <alfredo.barra@usach.cl>
2014-10-01 20:05:44 -0300

    Se seleccionan dos usuarios, y sólo se puede hablar entre ellos. Falta revisar validaciones, y colocar video al sistema. Igualmente, queda por realizar acciones en caso de desconexión, o cambio de cliente (cuando se hace clic en "Siguiente" y ya se estaba hablando con alguien).

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ChaRulet
 ========
 
 Proyecto de ChatRoulette para Sistemas Distribuídos
+
+Este laboratorio fue realizado por Alfredo Barra y Diego Vallejos.
+
+El programa fue desarrollado en node.JS; además, fueron utilizados los siguientes aditamentos:
+
+- ExpressJS: para el servicio http
+- Socket.IO: para la transmisión en tiempo real
+- OpenTok: para la creación de credenciales y streaming de voz y video
--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express'),
 	server	= require('http').createServer(app),	
 	io		= require('socket.io').listen(server),
 	opentok = require('opentok'),
-	ot =opentok.OpenTokSDK('45007202', '4d18fa6486b40c894e2a55a5e9defe89d8a1cdfa'),
-	nicknames = [];									
+	ot = opentok.OpenTokSDK('45007202', '4d18fa6486b40c894e2a55a5e9defe89d8a1cdfa'),
+	nicknames = [],
+	listaSesiones = [];
+	sesionesSolos = [];
+
 
 //hacer que el servidor escuche las peticiones en el puerto 3000
 server.listen(3000);
@@ -22,7 +25,7 @@ app.use(express.static(__dirname + '/'));
 
 //Cuando un cliente se conecte
 io.sockets.on('connection',function(socket){
-	console.log("Un cliente se ha conectado");
+	console.log("Un cliente se ha conectado");	
 
 	socket.on('entrar', function(data, callback){
 		if (nicknames.indexOf(data) != -1){
@@ -33,6 +36,8 @@ io.sockets.on('connection',function(socket){
 			socket.nickname = data;
 			console.log('El usuario '+socket.nickname+' se ha conectado');
 			nicknames.push(socket.nickname);
+			listaSesiones.push(socket);
+			sesionesSolos.push(socket.nickname);
 			//Le creamos una sesión al usuario
 			socket.emit('sesion', function(data){
 				ot.createSession('localhost', {}, function(session){
@@ -48,6 +53,60 @@ io.sockets.on('connection',function(socket){
 		}
 	});
 
+	//Cuando busque compañero
+	socket.on('siguiente', function(data, callback){
+		//El que busca pareja
+		/*var buscando ={
+			IDSesion: data.sessionID,
+			nCliente: socket.sessionID
+		};*/
 
+		//Si antes tenia un compañero
+		if(data != null){
 
+		}
+
+		for(var i = 0; i < sesionesSolos.length; i++){
+			//Si en la lista de solos hay alguien mas aparte de mi
+			if(sesionesSolos[i] != socket.nickname){
+				var tmpPartner = sesionesSolos[i];
+				//Eliminar al compañero de la lista de solitarios
+				sesionesSolos.splice(i, 1);
+				//Ahora me tengo que eliminar a mi
+				sesionesSolos.splice(sesionesSolos.indexOf(socket.nickname),1);
+				socket.companero = listaSesiones[nicknames.indexOf(tmpPartner)];
+				listaSesiones[nicknames.indexOf(tmpPartner)].companero = socket;				
+			}
+		};
+		//Si encontré a alguien para hablar
+		if(socket.companero){
+			callback(true);
+			//Me envío el nombre de mi compañero
+			socket.emit('suscribete', socket.companero.nickname);
+			//Y le envío mi nombre a mi compañero
+			socket.companero.emit('suscribete', socket.nickname);
+		}else{
+			callback(false);
+		};
+	});
+
+	//Cuando envíe mensaje
+	socket.on('mensaje', function(data){
+		var destino = data.destino;
+		var envio = data.mensaje;
+
+		//console.log('Enviando mensaje a ' + $destino + '\n Contenido: '+ $envio);
+		var datos = {
+			nick: socket.nickname,
+			mensaje: envio
+		};
+		//Le envío el mensaje a mi compañero
+		socket.companero.emit('mensaje', datos);
+	});
+
+	socket.on('disconnect', function(){
+		console.log('El usuario '+socket.nickname+' se desconectó')
+		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		listaSesiones.splice(listaSesiones.indexOf(socket),1);
+	})	
 });
