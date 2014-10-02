8036391009b0bbae8cc8b972be72c47156ba429c
aibarr <alfredo.barra@usach.cl>
2014-10-01 23:14:13 -0300

    Se agregan comentarios al codigo, se mejoran directivas de conexión. Falta bloquear la conversación entre dos usuarios.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express'),
 	server	= require('http').createServer(app),	
 	io		= require('socket.io').listen(server),
 	opentok = require('opentok'),
-	ot = opentok.OpenTokSDK('45007202', '4d18fa6486b40c894e2a55a5e9defe89d8a1cdfa'),
+	ot = new opentok.OpenTokSDK('45007202', '4d18fa6486b40c894e2a55a5e9defe89d8a1cdfa'),
 	nicknames = [],
 	listaSesiones = [];
 	sesionesSolos = [];
@@ -25,90 +25,112 @@ app.use(express.static(__dirname + '/'));
 
 //Cuando un cliente se conecte
 io.sockets.on('connection',function(socket){
-	console.log("Un cliente se ha conectado");	
+	console.log("Un cliente se ha conectado");
+	//Pongo la bandera que no tengo conversa ahora mismo
+	var compadre = 0;
 
+	//Cuando entro a mi sala de Chat
 	socket.on('entrar', function(data, callback){
+		//Si ya está mi nick registrado
 		if (nicknames.indexOf(data) != -1){
+			//me aviso que ya está mi nombre por ahi dando vueltas
 			callback(false);
 		}
-		else{			
+		else{
+			//Sino aviso que sí puedo entrar
 			callback(true);
+			//Guardo mi nick
 			socket.nickname = data;
 			console.log('El usuario '+socket.nickname+' se ha conectado');
+			//Lo pongo en un arreglo, y me registro en la lista de gente sola
 			nicknames.push(socket.nickname);
 			listaSesiones.push(socket);
 			sesionesSolos.push(socket.nickname);
+
+			var datos;
 			//Le creamos una sesión al usuario
-			socket.emit('sesion', function(data){
-				ot.createSession('localhost', {}, function(session){
-					data ={
-						sessionID: session.sessionID,
-						token: ot.generateToken({
-							sessionID: session.sessionID,
-							role: opentok.Roles.MODERATOR
-						})
-					};
-				});
-			});
+			/*ot.createSession('localhost', {}, function(session){
+				datos = {
+				 	sessionId: session.sessionId,
+					token: ot.generateToken({
+						sessionId: session.sessionId,
+						role: opentok.Roles.MODERATOR
+					})
+				};
+			});*/
+			//Enviamos las credenciales de sesión
+			socket.emit('sesion', datos	);
 		}
 	});
 
 	//Cuando busque compañero
 	socket.on('siguiente', function(data, callback){
-		//El que busca pareja
-		/*var buscando ={
-			IDSesion: data.sessionID,
-			nCliente: socket.sessionID
-		};*/
-
 		//Si antes tenia un compañero
 		if(data != null){
+			//Le aviso que se busque a otro
 			socket.companero.emit('desuscribir', {});
-			sesionesSolos.push(data);
-			sesionesSolos.push(socket.nickname);
+			sesionesSolos.push(data);			
 		}
+		sesionesSolos.push(socket.nickname);
 
+		//Ahora a buscar un compañero
 		for(var i = 0; i < sesionesSolos.length; i++){
 			//Si en la lista de solos hay alguien mas aparte de mi
-			if(sesionesSolos[i] != socket.nickname && sesionesSolos[i]!=data){
+			if(sesionesSolos[i] != socket.nickname && sesionesSolos[i]!=data){				
 				var tmpPartner = sesionesSolos[i];
 				//Eliminar al compañero de la lista de solitarios
 				sesionesSolos.splice(i, 1);
 				//Ahora me tengo que eliminar a mi
 				sesionesSolos.splice(sesionesSolos.indexOf(socket.nickname),1);
-				socket.companero = listaSesiones[nicknames.indexOf(tmpPartner)];
-				listaSesiones[nicknames.indexOf(tmpPartner)].companero = socket;				
+				socket.companero = listaSesiones[buscarSocket(listaSesiones,tmpPartner)];
+				listaSesiones[buscarSocket(listaSesiones,tmpPartner)].companero = socket;				
 			}
 		};
 		//Si encontré a alguien para hablar
 		if(socket.companero && data != socket.companero.nickname){
 			callback(true);
+			compadre = 1;
 			//Me envío el nombre de mi compañero
 			socket.emit('suscribete', socket.companero.nickname);
 			//Y le envío mi nombre a mi compañero
 			socket.companero.emit('suscribete', socket.nickname);
 		}else{
+			//Sino, me aviso que tengo que esperar
 			callback(false);
+			compadre = 0;
 		};
 	});
 
 	//Cuando envíe mensaje
 	socket.on('mensaje', function(data){
-		var destino = data.destino;
-		var envio = data.mensaje;
-
-		//console.log('Enviando mensaje a ' + $destino + '\n Contenido: '+ $envio);
 		var datos = {
 			nick: socket.nickname,
-			mensaje: envio
+			mensaje: data.mensaje
 		};
 		//Le envío el mensaje a mi compañero
 		socket.companero.emit('mensaje', datos);
 	});
 
+	//Cuando me desconecte
 	socket.on('disconnect', function(){
-		console.log('El usuario '+socket.nickname+' se desconectó')
+		console.log('Un usuario se desconectó\n');
+		//Si tenia un compañero
+		if(compadre) {
+			//Le digo a la consola que me fui
+			console.log('Se desconectó el usuario '+socket.nickname);
+			//Le aviso que me fui
+			socket.companero.emit('desuscribir', {});
+			//Y aviso que mi compañero está solo
+			sesionesSolos.push(socket.companero.nickname);
+		}
 		nicknames.splice(nicknames.indexOf(socket.nickname),1);
 		listaSesiones.splice(listaSesiones.indexOf(socket),1);
 	})	
 });
+
+function buscarSocket(arregloSocket, usuario){
+	for(var i = 0; i < arregloSocket.length; i++){
+		if(arregloSocket[i].nickname==usuario) return i;
+	}
+	return -1;
+};
